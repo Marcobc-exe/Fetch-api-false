901754d3196205a76f1d003babc7b17397ba3c1f
Marcobc-exe <marco.developing@gmail.com>
2022-10-17 17:19:44 -0300

    [Feat](Fetch API): Comments added to index.js explaining the process + preview web page image.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = document.getElementById("app");
 const titleWelcome = document.createElement("h1");
 titleWelcome.innerHTML = "Hello Vanilla JavaScript!";
 
+// Web page description
 const description = document.createElement("p")
 description.innerHTML = `
     This is a simple website where I make use a test false API =)
@@ -14,18 +15,22 @@ description.className = "description"
 app.appendChild(titleWelcome);
 app.appendChild(description);
 
+// Contains user of each data in a card
 const containerUserCard = document.createElement("div")
 containerUserCard.className = "containerUserCard"
 
+// Descriptive labels on user data
 const nameLabel = "Name: "
 const userEmailLabel = "Email: "
 const userCompanyNameLabel = "Name company: "
 
+// Fetch data user from jsonplaceholder
 const getUsers = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
     console.table(data);
 
+    // Generate elements with user data and insert it to the front end
     data.forEach((dataUsers) => {
         const userCard = document.createElement("div");
         userCard.className = "userCard";
